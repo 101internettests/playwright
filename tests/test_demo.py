90062caf789b1c84da611bd48e49b4ff544357a3
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-05-16 16:16:49 +0500

    Обновила тесты

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -8,22 +8,24 @@ def test_first(page: Page):
     expect(page.locator('#firstHeading')).to_have_text('Обсуждение Википедии:Содержание')
 
 
-# тест, который листает странички
+# Тест который отправляет заявку с попапа
 def test_pag(page: Page):
     page.goto('https://www.moskvaonline.ru/')
     change_region = page.locator('(//a[@href="/select-region"])[1]')
     change_region.click()
     change_balashixa = page.locator('//input[@placeholder="Введите первые 3 буквы"]')
     change_balashixa.fill('Балашиха')
-    choose_balashixa = page.locator('//a[contains(text(), "Балашиха (Московская область)")]')
+    choose_balashixa = page.locator('(//a[contains(text(), "Балашиха")])[1]')
     choose_balashixa.click()
-    CHOOSE_THE_COVERAGE_MAP = page.locator("//a[@datatest='main_address_downbutton']")
+    CHOOSE_THE_COVERAGE_MAP = page.locator("//a[contains(text(), 'Карта покрытия')]")
     CHOOSE_THE_COVERAGE_MAP.click()
-    CHOSE_REGION = page.locator("(//a[contains(text(), 'Красный')])[1]")
+    CHOOSE_ZONE = page.locator("//a[contains(text(), 'Балашиха')]")
+    CHOOSE_ZONE.click()
+    CHOSE_REGION = page.locator("(//a[contains(text(), 'Красный Текстильщик ул')])[1]")
     CHOSE_REGION.click()
-    STREET = page.locator("//a[contains(text(), 'Советская ул')]")
-    STREET.click()
-    HOUSE = page.locator("//a[contains(text(), '10')]")
+    # STREET = page.locator("//a[contains(text(), 'Советская ул')]")
+    # STREET.click()
+    HOUSE = page.locator("//a[contains(text(), '3А')]")
     HOUSE.click()
     INPUT = page.locator("//input[@datatest='rates_popup1_from_quiz_input_tel']")
     INPUT.fill('1111111111')
@@ -31,9 +33,4 @@ def test_pag(page: Page):
     CHOW_RES.click()
     CLOSE_POPUP = page.locator("//div[@datatest='close_popup1_from_quiz_input_tel']")
     CLOSE_POPUP.click()
-    scroll = page.locator("//div[contains(text(), 'Отзывы о Москва Онлайн на Яндекс Картах')]")
-    scroll.scroll_into_view_if_needed()
-    PANGINATION = page.locator("//a[@href='/moskovskaya-oblast/doma-nzl/2']")
-    PANGINATION.click()
-    CONNECT = page.locator("(//span[contains(text(), 'Подключить по акции')])[2]")
-    page.type('input[id = "fix_callback_phone"]', '1111111111')
+
