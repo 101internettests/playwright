1a74d758ec1ffe23723e4348651961562c6d8842
zhzh25 <zhzh2592@mail.ru>
2024-07-25 17:54:38 +0300

    добавлен тест по футеру и исправлен хедер

---
+++ pages/main_site_pages/main_page_click.py
@@ -0,0 +1,42 @@
+import pytest
+from playwright.sync_api import Page, expect
+from time import sleep
+
+
+def check_header(page: Page):
+    page.locator('(//a[@datatest="main_internet_button"])[1]').click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.get_by_role("link", name="провайдеры", exact=True).click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.get_by_role("link", name="рейтинг", exact=True).click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.get_by_role("link", name="Тарифы", exact=True).click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.locator('(//a[contains(text(), "Интернет в офис")])[1]').click()
+
+
+def check_footer(page: Page):
+    page.get_by_role("link", name="О нас").click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.get_by_role("link", name="Оплата и гарантии").click()
+    page.get_by_role("link", name="Подключить интернет").click()
+    page.locator('//a[contains(text(), "Сотрудничество")]').click()
+    page.goBack()
+    page.get_by_role("link", name="Документы").click()
+    page.get_by_role("contentinfo").locator("li").filter(has_text="Политика обработки персональных данных").click()
+    page.get_by_role("link", name="Карта сайта").click()
+
+
+def check_career(page: Page):
+    page.goto('https://101internet.ru/abakan')
+    page.get_by_role("link", name="Карьера").click()
+
+
+def check_blog(page: Page):
+    page.goto('https://101internet.ru/abakan')
+    page.get_by_role("link", name="Блог").click()
+
+
+def check_rewievs(page: Page):
+    page.goto('https://101internet.ru/abakan')
+    page.get_by_role("link", name="Отзывы о компании").click()
--- tests/101/abakan/test_101_click.py
+++ tests/101/abakan/test_101_click.py
@@ -1,8 +1,16 @@
 import pytest
 from playwright.sync_api import Page, expect
-from pages.main_site_pages.abakan_page_click import check_header_abakan
+from pages.main_site_pages.main_page_click import check_header, check_footer, check_rewievs, check_blog, check_career
 
 
 def test_header_abakan(page: Page):
     page.goto('https://101internet.ru/abakan')
-    check_header_abakan(page)
+    check_header(page)
+
+
+def test_footer_abakan(page: Page):
+    page.goto('https://101internet.ru/abakan')
+    check_footer(page)
+    check_rewievs(page)
+    check_blog(page)
+    check_career(page)
