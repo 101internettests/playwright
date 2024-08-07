3301aeeb2e3b767ad43ca07fd5b26ea003eccd2b
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-08-07 18:00:23 +0500

    Новые тесты

--- pages/MOL/moscow_page.py
+++ pages/MOL/moscow_page.py
@@ -25,3 +25,20 @@ def check_tags(page: Page):
     expect(page.locator('(//a[@href="/rates/internet-300-mbit"])[1]')).to_be_visible()
     expect(page.locator('(//a[@href="/rates/internet-500-mbit"])[1]')).to_be_visible()
     expect(page.locator('(//a[@href="/rates/online-kinoteatr"])[1]')).to_be_visible()
+
+
+def operatory_mts_main(page: Page):
+    expect(page.get_by_role("link", name="Подключить интернет")).to_be_visible()
+    expect(page.get_by_role("link", name="Мобильные операторы")).to_be_visible()
+    expect(page.locator('//span[contains(text(), "МТС")]')).to_be_visible()
+    expect(page.locator('//span[contains(text(), "Тарифы")]')).to_be_visible()
+    expect(page.locator('//img[@itemprop="contentUrl"]')).to_be_visible()
+    expect(page.locator("#operator_banner").get_by_text("Подключение")).to_be_visible()
+    expect(page.locator("#operator_banner").get_by_text("Техподдержка")).to_be_visible()
+    expect(page.locator('//a[contains(text(), "+7  (800)  250-08-90")]')).to_be_visible()
+    expect(page.locator('//a[contains(text(), "+7 (800) 250-08-90")]')).to_be_visible()
+    expect(page.get_by_role("link", name="Об операторе")).to_be_visible()
+    expect(page.get_by_role("link", name="Тарифы").nth(1)).to_be_visible()
+    expect(page.get_by_role("link", name="Номера")).to_be_visible()
+    expect(page.get_by_role("link", name="акции", exact=True)).to_be_visible()
+    expect(page.get_by_role("link", name="в 1")).to_be_visible()
